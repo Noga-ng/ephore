217aae495e9b9991ee14e496b0a92549f3ac205c
noga <198326931+Noga-ng@users.noreply.github.com>
2026-09-21 08:13:53 +0300

    Remove splash input from rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -41,8 +41,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        splash: resolve(__dirname, 'splash.html'),
+        main: resolve(__dirname, 'index.html')
       },
     },
   },
